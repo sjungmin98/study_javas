d47c73172ba56b8a99397dfa11b9b9d43186e701
sjungmin98 <shc980@gmail.com>
2024-03-25 06:15:29 +0000

    11

--- src/Loopsfors.java
+++ src/Loopsfors.java
@@ -1,5 +1,5 @@
 // refer : https://www.w3schools.com/java/java_for_loop.asp
-public class Loops_fors {
+public class Loopsfors {
     public static void main(String[] args){
         /* 
             for 동작 위한 조건 : 시작점, 종료 조건, 반복 횟수
---
+++ src/quests/WhilesIfsBreak.java
@@ -0,0 +1,31 @@
+package quests;
+
+import java.util.Scanner;
+
+public class WhilesIfsBreak {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("점수를 입력하세요. (종료하려면 -1 입력): ");
+            int score = scanner.nextInt();
+            
+            if (score == -1) {
+                break;
+            }
+            
+            if (score >= 90) {
+                System.out.println("A");
+            } else if (score >= 80) {
+                System.out.println("B");
+            } else if (score >= 70) {
+                System.out.println("C");
+            } else {
+                System.out.println("F");
+            }
+        }
+        
+        System.out.println("프로그램 종료");
+        scanner.close();
+    }
+}
