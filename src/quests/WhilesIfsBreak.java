a40a9888597205ef21465e512aa3c4a49d61f3a7
sjungmin98 <shc980@gmail.com>
2024-03-25 06:18:39 +0000

    11

--- src/quests/WhilesIfsBreak.java
+++ src/quests/WhilesIfsBreak.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class WhilesIfsBreak {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean whileFlag = true;
         
-        while (true) {
+        while (whileFlag) {
             System.out.println("점수를 입력하세요. (종료하려면 -1 입력): ");
             int score = scanner.nextInt();
             
             if (score == -1) {
-                break;
+                whileFlag = false;
+                continue;
             }
             
             if (score >= 90) {
@@ -23,7 +25,6 @@ public class WhilesIfsBreak {
             } else {
                 System.out.println("F");
             }
-        }
         
         System.out.println("프로그램 종료");
         scanner.close();
